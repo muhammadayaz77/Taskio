cb94c8f696e7ee31703239676bf56a06ec20d211
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2025-12-29 15:44:49 +0500

    add morgan for logging; update server configuration with error handling and not found middleware

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,16 +1,21 @@
 import express from "express";
+import morgan from 'morgan'
 const app = express();
 import dotenv from "dotenv";
 import { DbConnection } from "./config/db.js";
 import cookieParser from 'cookie-parser';
 dotenv.config();
 import cors from 'cors';
+const PORT = process.env.PORT || 3000
 
 
 app.use(cors({
   origin: 'http://localhost:5173', 
+  methods : ['GET','POST','DELETE','PUT'],  
+  allowedHeaders : ['Content-Type','Authorization'],
   credentials: true 
 }))
+app.use(morgan('dev'))
 
 
 app.use(cookieParser())
@@ -35,9 +40,20 @@ app.use("/api/v1/task" , taskRoutes)
 import developerRoutes from './routes/developer.route.js'
 app.use("/api/v1/developer" , developerRoutes)
 
+// error middleware
+app.use((err,req,res,next) => {
+  console.log("Error : ",err.stack)
+   res.status(500).json({
+    message : "Interval server error"
+   })
+})
 
-
-const PORT = process.env.PORT || 3000
+// not found middleware
+app.use((req,res,) => {
+   res.status(404).json({
+    message : "Not Found"
+   })
+})
 
 // database connection 
 DbConnection()
