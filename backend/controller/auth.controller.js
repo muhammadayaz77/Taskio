8e1ee09503c01708ab1f0a9ebef217b1ec8a3581
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2026-01-08 15:36:53 +0500

    enhance login logic; add email verification check and resend verification email if needed

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -77,66 +77,92 @@ export const register = async (req, res) => {
 };
 export const login = async (req, res) => {
   try {
-    const { fullName,email,password } = req.body;
-    
-    const decision = await aj.protect(req, { email }); // Deduct 5 tokens from the bucket
-    console.log("Arcjet decision", decision);
-    
-    if (decision.isDenied()) {
-      res.writeHead(403, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ message: "Invalid Email address" }));
-    }
+    const {email,password } = req.body;
 
-    let existUser = await User.findOne({email});
-    console.log("User")
-    // console.log(existUser);
-    if(existUser){
+    const user = await User.findOne({email}).select("+password");
+
+    if(!user){
       return res.status(400).json({
-        message : "Email address already in use",
+        message : 'Invalid email or password',
+        success : false
+      })
+    }
+    if(!user.isEmailVerified){
+        const existingVerification = await Verification.findOne({
+          userId : user._id
+        })
+        if(existingVerification && existingVerification.expiresAt > new Date().now){
+          return res.status(400).json({
+            message : 'Email not verified.Please check your email for the verification link.  ',
+            success : false
+          })
+        }
+        else {
+          await Verification.findByIdAndDelete(existingVerification._id)
+          const verificationToken = jwt.sign({
+            userId : user._id,purpose : 'email-verification'
+          },
+         process.env.JWT_SECRET,
+         {
+          expiresIn : '1h'
+         }
+        );
+        await Verification.create({
+          userId : user._id,
+          token : verificationToken,
+          expiresAt : new Date(Date.now() * 1 * 60 * 60 * 1000)
+        })
+        const verificationLink = `${process.env.FRONTEND_URL}/verify-email/${verificationToken}`; 
+        const emailBody = `<p>Click <a href="${verificationLink}">here</a> to verify your email</p>`;
+        const emailSubject = "Verify your email"
+  
+        const isEmailSent = await sendEmail(email,emailSubject,emailBody)
+        console.log("isemail sent : ",isEmailSent)
+        if(!isEmailSent){
+          return res.status(500).json({
+            message : 'Failed to send verification email',
+            success : false
+          })
+        }
+      return res.status(201).json({ 
+        message: 'Verification email sent to your email. Please check and verify your account.',
+        success : false
+      });
+        }
+      }
+
+    let isPasswordValid = await bcrypt.compare(password,user.password)
+    if(!isPasswordValid){
+      return res.status(400).json({
+        message : 'Invalid email or password',
         success : false
       })
     }
 
-    // const managerId = req.user._id;
-    let newUser = await User.create({
-      name : fullName,
-      email,
-      password
-    });  
-    const user = {
-      name : newUser.name,
-      email : newUser.email,
-    }
+    let token = jwt.sign({
+      userId : user._id,
+      purpose : 'login',
+    },
+   process.env.JWT_SECRET,
+   {
+    expiresIn : '7d'
+   }
+  );
 
-    const verificationToken = jwt.sign(
-      {userId:newUser._id,purpose : 'email-verification'},
-      process.env.JWT_SECRET,
-      {expiresIn : '1h'}
-    )
+  user.lastLogin = new Date();
+  await user.save();
 
-    await Verification.create({
-      userId : newUser._id,
-      token : verificationToken,
-      expiresAt : new Date(Date.now() + 1 * 60 * 60 * 1000)
+  const userDate = user.toObject();
+  delete userDate.password
+   
+    return res.status(200).json({
+      message : 'Your are logged in',
+      token,
+      user : userDate,
+      success : true
     })
 
-    // send email
-      const verificationLink = `${process.env.FRONTEND_URL}/verify-email/token=${verificationToken}`; 
-      const emailBody = `<p>Click <a href="${verificationLink}">here</a> to verify your email</p>`;
-      const emailSubject = "Verify your email"
-
-      const isEmailSent = await sendEmail(email,emailSubject,emailBody)
-      console.log("isemail sent : ",isEmailSent)
-      if(!isEmailSent){
-        return res.status(500).json({
-          message : 'Failed to send verification email',
-          success : false
-        })
-      }
-    return res.status(201).json({ 
-      message: 'Verification email sent to your email. Please check and verify your account.',
-      success : false
-    });
+      
   } catch (err) {
     res.status(500).json({ 
       message: 'Internal Server error',
@@ -144,8 +170,7 @@ export const login = async (req, res) => {
     
     });
   }
-};
-
+}
 
 export const verifyEmail = async (req, res) => {
         try {
