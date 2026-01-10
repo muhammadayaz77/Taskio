1ca0012fc244518ce25f1f7d09c8cde4124b0339
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2026-01-10 22:15:54 +0500

    fix: enhance email verification process by adding error handling for email sending

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -37,12 +37,26 @@ export const register = async (req, res) => {
       name : newUser.name,
       email : newUser.email,
     }
-
+    
+    // send email
     const verificationToken = jwt.sign(
       {userId:newUser._id,purpose : 'email-verification'},
       process.env.JWT_SECRET,
       {expiresIn : '1h'}
     )
+      const verificationLink = `${process.env.FRONTEND_URL}/verify-email/${verificationToken}`; 
+      const emailBody = `<p>Click <a href="${verificationLink}">here</a> to verify your email</p>`;
+      const emailSubject = "Verify your email"
+    const isEmailSent = await sendEmail(email,emailSubject,emailBody)
+    console.log("isemail sent : ",isEmailSent);
+    
+    if(!isEmailSent){
+      return res.status(500).json({
+        message : 'Failed to send verification email',
+        success : false
+      })
+    }
+    
 
     await Verification.create({
       userId : newUser._id,
@@ -50,19 +64,6 @@ export const register = async (req, res) => {
       expiresAt : new Date(Date.now() + 1 * 60 * 60 * 1000)
     })
 
-    // send email
-      const verificationLink = `${process.env.FRONTEND_URL}/verify-email/${verificationToken}`; 
-      const emailBody = `<p>Click <a href="${verificationLink}">here</a> to verify your email</p>`;
-      const emailSubject = "Verify your email"
-
-      const isEmailSent = await sendEmail(email,emailSubject,emailBody)
-      console.log("isemail sent : ",isEmailSent)
-      if(!isEmailSent){
-        return res.status(500).json({
-          message : 'Failed to send verification email',
-          success : false
-        })
-      }
     return res.status(201).json({ 
       message: 'Verification email sent to your email. Please check and verify your account.',
       success : false
