0d77feac1b975b20e59a9f16978f755fdeaac442
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2026-01-22 15:30:02 +0500

    feat: remove team model and add workspace model with schema definition

---
+++ backend/models/workspace.model.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const workspaceSchema = new mongoose.Schema({
+  name: { type: String, required: true, trim: true },
+  description:{
+    type: String, trim: true
+  },
+  color: {
+    type: String, trim: true
+},
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref:'User',
+    required:true
+},
+  members: [{
+    user: {type: mongoose.Schema.Types.ObjectId,
+    ref:'User'
+  },
+  role : {
+    type: String, 
+    emun: ["owner","member"], 
+    default: "member"
+  },
+  jointAt : {
+    type: Date,
+    default : Date.now
+  },
+  projects : [{
+    type : mongoose.Schema.Types.ObjectId,
+    ref :'Project'   
+  }]
+}],
+
+}, { timestamps: true });
+
+const Team = mongoose.model('Team', teamSchema);
+export default Team;
