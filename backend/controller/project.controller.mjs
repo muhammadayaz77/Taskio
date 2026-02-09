14a02fad39a4e6ccbd8762ba4021e4dec12aa275
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2026-02-09 15:18:47 +0500

    complete controller of projects

--- backend/controller/project.controller.mjs
+++ backend/controller/project.controller.mjs
@@ -1,34 +1,59 @@
+import Project from "../models/projects.model.mjs";
+import Workspace from "../models/workspace.model.mjs";
 
 
 export const createProject = async (req, res) => {
   try {
+    const {workspaceId} = req.params;
     const { 
-      name,
+      title,
       description,
-      color
+      status,
+      startDate,
+      dueDate,
+      tags,
+      members 
      } = req.body;
 
-    const workspace = await Workspace.create({
-      name,
-      description,
-      color,
-      owner:req.user._id,
-      members : [
-        {
-          user : req.user._id,
-          role : 'owner',
-          joinedAt : new Date()
-        }
-      ]
-    })
+     const workspace = await Workspace.findById(workspaceId)
     
+     if(!workspace){
+      return res.status(404).json({
+      message: "Workspace not found",
+      success : false
+    });
+     }
 
+     const isMember = workspace.members.some((member) => member.user.toString() === req.user._id.toString())
+
+     if(!isMember){
+      return res.status(404).json({
+      message: "You are not a member of this workspace",
+      success : false
+    });
+     }
    
 
+     const tagsArray = tags ? tags.split(',') : []
+
+     const newProject = await Project.create({
+        title,
+      description,
+      status,
+      startDate,
+      dueDate,
+      tags : tagsArray,
+      members,
+      createdBy:req.user._id
+     })
+
+     workspace.projects.push(newProject._id);
+     await workspace.save()
     return res.status(201).json({
-      message: "Workspace created successfully",
-      workspace
+      message: "Project created successfully",
+      newProject
     });
+  
   } catch (err) {
     console.log("Error : ", err);
     res.status(500).json({
