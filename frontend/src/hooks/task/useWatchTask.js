37e3fc68845759cbefbd5aaadb9541b79fa24b20
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2026-03-02 15:23:44 +0500

    add hooks for archieved and watchers

---
+++ frontend/src/hooks/task/useWatchTask.js
@@ -0,0 +1,36 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateData } from "../../api/axios";
+
+ const useWatchTask = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ taskId }) =>
+      updateData(`/tasks/${taskId}/title`, {}),
+
+    onSuccess: (response, variables) => {
+      // ✅ invalidate correct task
+      queryClient.invalidateQueries({
+        queryKey: ["task", variables.taskId],
+      });
+      queryClient.invalidateQueries({
+        queryKey: ["task-activity", variables.taskId],
+      });
+
+      window.toastify(
+        response?.message || "Task updated successfully",
+        "success"
+      );
+    },
+
+    onError: (err) => {
+      window.toastify(
+        err?.response?.data?.message || "Task update failed",
+        "error"
+      );
+    },
+  });
+};
+
+
+export default useWatchTask
